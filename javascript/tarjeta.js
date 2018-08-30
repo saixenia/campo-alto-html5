26993ae670a71029ca8660267a8deb3779578209
saixenia <julian-rodriguez92@hotmail.com>
2018-08-30 17:42:17 -0500

    modificacion de texto,las tarjetas ,colores

--- javascript/tarjeta.js
+++ javascript/tarjeta.js
@@ -6,6 +6,8 @@
 window.addEventListener('load', function () {
     var contenedor = document.getElementById('contenedor');
     var botonCrear = this.document.getElementById('crear');
+    var texto = this.document.getElementById('texto');
+    var color = this.document.getElementById('color');
     // con esto hace referencia al elemento
     // var tarjeta = document.getElementById('tarjeta');
     botonCrear.addEventListener('click', function () {
@@ -13,10 +15,17 @@ window.addEventListener('load', function () {
         var tarjeta = document.createElement('div');
         // agrgar un clase
         tarjeta.classList.add('tarjeta');
+        //agrrgar el texto
+        tarjeta.innerText = texto.value;
+        texto.value = "";
+        //cambiar color
+        tarjeta.style.backgroundColor = color.value;
         // agragasr como hijo al contenedor
         contenedor.appendChild(tarjeta);
         //mostrar en consola
         //        console.log(tarjeta);
+
+
     });
 });
 
